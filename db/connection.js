5290f0571c5e4484fb321fffcb5d78d50a48568d
andfwas <andfwas@gmail.com>
2017-10-19 09:24:51 -0600

    first heroku deploy

--- db/connection.js
+++ db/connection.js
@@ -1,3 +1,5 @@
+const environment = process.env.NODE_ENV || 'development'
+
 var environment = process.env.NODE_ENV || 'development'
 var config = require('../knexfile.js')[environment]
-module.exports = require('knex')(config)
+module.exports = require('knex')[config]
--- knexfile.js
+++ knexfile.js
@@ -1,19 +1,15 @@
 // Update with your config settings.
+require('dotenv').config()
 
 module.exports = {
 
   development: {
     client: 'pg',
-    connection: {
-      database: 'gSecrets'
-    }
+    connection: 'postgres://llocalhost/gSecrets'
   },
 
   production: {
     client: 'pg',
-    connections: {
-      database: process.env.DATABASE_URL
-    }
+    connection: process.env.DATABASE_URL
   }
-
 };
