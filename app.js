c432e985d4f0eff8e1e4572f0b1d125c74d61d7a
andfwas <andfwas@gmail.com>
2017-10-19 10:51:13 -0600

    express static path join dirname public, css is linked properly

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const jsonParser = bodyParser.json()
+const path = require('path')
 const app = express()
 
 const port = process.env.PORT || 3000
@@ -11,6 +12,7 @@ app.set('view engine', 'hbs')
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res, next) => {
   res.render('index')
